78278056a2726a78b3cf0654969edf4cebb674e8
Кирилл Зюзев <super.yttt2017@yandex.ru>
2022-06-03 19:50:22 +0500

    Добавьте файлы проекта.

---
+++ MyProject/wwwroot/scripts/Carousel.js
@@ -0,0 +1,44 @@
+﻿var angle = 0;
+function galleryspin(sign) {
+    spinner = document.querySelector("#spinner");
+    if (!sign)
+    { angle = angle + 45; }
+    else
+    { angle = angle - 45; }
+    spinner.setAttribute("style", "-webkit-transform: rotateY(" + angle + "deg); -moz-transform: rotateY(" + angle + "deg); transform: rotateY(" + angle + "deg);");
+}
+
+//let sliderContainer = document.querySelector("#carousel");
+//let innerSlider = document.querySelector("#spinner");
+
+//let pressed = false;
+//let startX;
+//let x;
+
+//sliderContainer.addEventListener("mousedown", (e) => {
+//    pressed = true;
+
+//    startX = e.offsetX;
+
+//    sliderContainer.style.cursor = "grabbing";
+//});
+
+//sliderContainer.addEventListener("mouseup", () => {
+//    sliderContainer.style.cursor = "grab";
+//    pressed = false;
+
+    
+
+//});
+
+//sliderContainer.addEventListener("mousemove", (e) => {
+//    if (!pressed) return;
+//    e.preventDefault();
+
+//    x = e.offsetX;
+//    if (startX - x > 0)
+//        galleryspin(true);
+//    else
+//        galleryspin(false);
+    
+//});
