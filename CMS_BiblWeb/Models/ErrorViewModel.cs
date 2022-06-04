3e9a2dd268d616527df3a31ba804d5024ed7daa1
Кирилл Зюзев <super.yttt2017@yandex.ru>
2022-06-04 22:26:50 +0500

    Адаптация сайта для библиотеки

--- CMS_BiblWeb/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace CMS_BiblWeb.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
