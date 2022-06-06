aa8f51cd815609505fc6a10104d07587b39299ab
Кирилл Зюзев <super.yttt2017@yandex.ru>
2022-06-06 17:24:29 +0500

    Внедерение записки, и файла для восстановления БД

--- MyProject/wwwroot/scripts/Carousel.js
+++ MyProject/wwwroot/scripts/Carousel.js
@@ -7,38 +7,3 @@ function galleryspin(sign) {
     { angle = angle - 45; }
     spinner.setAttribute("style", "-webkit-transform: rotateY(" + angle + "deg); -moz-transform: rotateY(" + angle + "deg); transform: rotateY(" + angle + "deg);");
 }
-
-//let sliderContainer = document.querySelector("#carousel");
-//let innerSlider = document.querySelector("#spinner");
-
-//let pressed = false;
-//let startX;
-//let x;
-
-//sliderContainer.addEventListener("mousedown", (e) => {
-//    pressed = true;
-
-//    startX = e.offsetX;
-
-//    sliderContainer.style.cursor = "grabbing";
-//});
-
-//sliderContainer.addEventListener("mouseup", () => {
-//    sliderContainer.style.cursor = "grab";
-//    pressed = false;
-
-    
-
-//});
-
-//sliderContainer.addEventListener("mousemove", (e) => {
-//    if (!pressed) return;
-//    e.preventDefault();
-
-//    x = e.offsetX;
-//    if (startX - x > 0)
-//        galleryspin(true);
-//    else
-//        galleryspin(false);
-    
-//});
